56c1537b1738da413882f156de6ab3c422381d8b
Rodgers31 <otienor986@gmail.com>
2021-08-09 10:14:42 -0400

    two test complete

--- api/server.js
+++ api/server.js
@@ -1,7 +1,39 @@
 // BUILD YOUR SERVER HERE
 const express = require('express');
+const User = require('./users/model');
 
 const server = express();
+
+server.get('/api/users', (req, res) => {
+	User.find()
+		.then((users) => {
+			res.json(users);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				message: 'error getting users',
+				err: err.message,
+			});
+		});
+});
+
+server.get('/api/users/:id', (req, res) => {
+	User.findById(req.params.id)
+		.then((user) => {
+			if (!user) {
+				res.status(404).json({
+					message: 'The user with the specified ID does not exist',
+				});
+			}
+			res.json(user);
+		})
+		.catch((err) => {
+			res.status(500).json({
+				message: 'error getting users',
+				err: err.message,
+			});
+		});
+});
 server.use('*', (req, res) => {
 	res.status(404).json({
 		message: 'not found',
